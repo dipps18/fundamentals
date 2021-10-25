72688533f75f08a580b861218c49f3bf2e08675b
dipsthedestroya <dipupurecha01@gmail.com>
2021-10-25 15:37:10 +0400

    updated readme.md and binary_search.md

--- binary_search_trees/README.md
+++ binary_search_trees/README.md
@@ -1,1 +1,26 @@
+#Classed and methods
+
+1) class Node: It stores the data as well as left and right children of a tree.
+
+2) Tree class: It accepts an array when initialized. The Tree class has a root attribute which returns the value of #build_tree.
+
+3) #build_tree: It takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed. The #build_tree method returns the level-0 root node.
+
+4) #insert: method which accepts a value to insert to the tree.
+
+5) #delete:  method which accepts a value to delete from the tree.
+
+6) #find: method which accepts a value and returns the node with the given value.
+
+7) #level_order:  method accepts a block and traverses the tree in breadth-first level order and yield each node to the provided block. The method returns an array of values if no block is given.
+
+8) #inorder, #preorder, and #postorder methods that accepts a block, traverse the tree in their respective depth-first order and yield each node to the provided block. The methods return an array of values if no block is given.
+
+9) #height: method accepts a node and returns its height. Height is defined as the number of edges in longest path from a given node to a leaf node.
+
+10) #depth: method accepts a node and returns its depth. Depth is defined as the number of edges in path from a given node to the tree’s root node.
+
+11) #balanced?: method checks if the tree is balanced. A balanced tree is one where the difference between heights of left subtree and right subtree of every node is not more than 1.
+
+12) #rebalance: method rebalances an unbalanced tree.
 
--- binary_search_trees/binary_search_trees.rb
+++ binary_search_trees/binary_search_trees.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class Tree
   def initialize(array)
     @root = build_tree(array.uniq.sort)
@@ -19,10 +19,100 @@ class Tree
     root
   end
 
+  def delete(val, parent = nil, root = @root, is_left_child = false)
+    return if root == nil
+    if root.data == val
+      if root.left == nil && root.right == nil
+        is_left_child ? parent.left = nil : parent.right = nil
+      elsif root.left && !root.right
+        is_left_child ? parent.left = root.left : parent.right = root.left
+      elsif root.right && !root.left
+        is_left_child ? parent.left = root.right : parent.right = root.right
+      else
+        root.data  = delete_helper(root.right, root, val)
+      end
+      return root.data
+    end
+    val > root.data ? delete(val, root, root.right, false) : delete(val, root, root.left, true)
+  end
+
+  def delete_helper(root, parent, val)
+    if root.left == nil
+      if parent.data != val
+        root.right ? parent.left = root.right : parent.left = nil
+      else
+        parent.right = root.right
+      end
+      return root.data
+    end
+    delete_helper(root.left, root, val) if root.left
+  end
+
+  def inorder(root = @root, arr = [], &block)
+    return if root == nil
+    inorder(root.left, arr, &block)
+    block_given? ? (yield root) : arr.push(root.data)
+    inorder(root.right, arr, &block)
+    return arr unless block_given? 
+  end
+
+  def preorder(root = @root, arr = [], &block)
+    return if root == nil
+    block_given? ? (yield root) : arr.push(root.data)
+    preorder(root.left, arr, &block)
+    preorder(root.right, arr, &block)
+    return arr unless block_given? 
+  end
+
+  def postorder(root = @root, arr = [], &block)
+    return if root == nil
+    postorder(root.left, arr, &block)
+    postorder(root.right, arr, &block)
+    block_given? ? (yield root) : arr.push(root.data)
+    return arr unless block_given?
+  end
+
+  def level_order(queue = [@root], arr =[], &block)
+    return if queue.empty?
+    block_given? ? (yield queue[0]) : arr.push(queue[0].data)
+    queue.push(queue[0].left) if queue[0].left
+    queue.push(queue[0].right) if queue[0].right
+    queue.shift
+    level_order(queue, arr, &block)
+    return arr unless block_given?
+  end
+
+  def find(value, root = @root)
+    return root if  root == nil || root.data == value
+    value > root.data ? find(value, root.right) : find(value, root.left)
+  end
+
+  def height(root, level = -1, count = -1)
+    return count if root == nil
+    level = level + 1
+    count = level if level > count 
+    count = height(root.left, level, count)
+    count = height(root.right, level, count)
+    level = level - 1
+    count
+  end
+
+  def depth(node)
+    height(@root) - height(node)
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def balanced?
+    (height(@root.left) - height(@root.right)).abs <= 1 
+  end
+
+  def rebalance
+    @root = build_tree(level_order.uniq.sort) unless balanced?
   end
 end
 
@@ -34,11 +124,26 @@ class Node
   end
 end
 
-t1 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-t1.pretty_print
-t1.insert(20)
-t1.insert(6666)
-t1.insert(0)
-t1.pretty_print
+t = Tree.new(Array.new(15){rand(1..100) })
+t.balanced?
+t.pretty_print
+t.inorder{|node| print "#{node.data} -> "}
+t.preorder{|node| print "#{node.data} -> "}
+t.postorder{|node| print "#{node.data} -> "}
+
+150.times{t.insert(rand(1..100))}
+t.pretty_print
+t.balanced?
+t.rebalance
+t.pretty_print
+t.balanced?
+100.times{t.delete(rand(1..100))}
+t.pretty_print
+t.balanced?
+t.rebalance
+t.pretty_print
 
 
+t.inorder{|node| print "#{node.data} -> "}
+t.preorder{|node| print "#{node.data} -> "}
+t.postorder{|node| print "#{node.data} -> "}
